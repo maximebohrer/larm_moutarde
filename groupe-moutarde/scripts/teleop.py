fb041295c63acf28b25b59853fe3d9f84aacf0cf
Nathanaël <toto@toto.fr>
2021-12-15 10:22:40 +0100

    .

---
+++ groupe-moutarde/scripts/teleop.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import math, rospy
+from geometry_msgs.msg import Twist
+
+# Initialize ROS::node
+rospy.init_node('move', anonymous=True)
+
+commandPublisher = rospy.Publisher(
+    '/cmd_vel',
+    Twist, queue_size=10
+)
+
+commandListener = rospy.Subscriber("/scan, sensor_msgs/LaserScan", perception)
+
+def perception(data):
+    c = 1
+
+
+
+# Publish velocity commandes:
+def move_command(data):
+    # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
+    cmd= Twist()
+    cmd.linear.x = 0.1
+    commandPublisher.publish(cmd)
+
+# call the move_command at a regular frequency:
+rospy.Timer(rospy.Duration(0.1), move_command, oneshot=False)
+
+# spin() enter the program in a infinite loop
+print("Start teleop.py")
+rospy.spin()
