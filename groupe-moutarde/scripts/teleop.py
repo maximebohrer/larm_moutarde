93a382780cbf55eaddb0261ecd25139b313965fa
Nathanaël <toto@toto.fr>
2021-12-15 11:44:31 +0100

    Version fonctionnelle du challenge1

--- groupe-moutarde/scripts/teleop.py
+++ groupe-moutarde/scripts/teleop.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
 
-import math, rospy
+import math, rospy, random
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 
 # Initialize ROS::node
 rospy.init_node('move', anonymous=True)
+
+obstacles=[]
 
 commandPublisher = rospy.Publisher(
     '/cmd_vel',
     Twist, queue_size=10
 )
 
-commandListener = rospy.Subscriber("/scan, sensor_msgs/LaserScan", perception)
-
 def perception(data):
-    c = 1
+    global obstacles
+    obstacles = data.ranges
+    #angle_min = data.angle_min
+    #angle_max = data.angle_max
+    #angle_inc = data.angle_increment
+    move_command(0)
 
-
+commandListener = rospy.Subscriber("/scan", LaserScan , perception)
 
 # Publish velocity commandes:
 def move_command(data):
     # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
     cmd= Twist()
-    cmd.linear.x = 0.1
+    rand = random.randint(0,30)
+    if min(obstacles) < 0.3:
+        cmd.angular.z = 0.5
+    elif min(obstacles[:100]) < 0.5:
+        cmd.linear.x = 0.3
+        cmd.angular.z = 0.5
+    else:
+        cmd.linear.x = 0.3
     commandPublisher.publish(cmd)
 
 # call the move_command at a regular frequency:
