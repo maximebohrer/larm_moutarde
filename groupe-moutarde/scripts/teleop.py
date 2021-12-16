4e9021c1f73bd9a53cfbd58e9368fa344dbe240a
maxime <maxime>
2021-12-16 11:33:42 +0100

    ok

--- groupe-moutarde/scripts/teleop.py
+++ groupe-moutarde/scripts/teleop.py
@@ -7,7 +7,7 @@ from sensor_msgs.msg import LaserScan
 # Initialize ROS::node
 rospy.init_node('move', anonymous=True)
 
-obstacles=[]
+obstacles=[0.1]
 
 commandPublisher = rospy.Publisher(
     '/cmd_vel',
@@ -29,16 +29,17 @@ def move_command(data):
     # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
     cmd= Twist()
     rand = random.randint(0,30)
-    if min(obstacles) < 0.3:
-        cmd.angular.z = 0.5
-    elif min(obstacles[:100]) < 0.5:
-        cmd.linear.x = 0.3
-        cmd.angular.z = 0.5
-    elif min(obstacles[-100:]) < 0.5:
-        cmd.linear.x = 0.3
-        cmd.angular.z = -0.5
+    print(min(obstacles[150:-150]))
+    if min(obstacles[150:-150]) < 0.3:
+        cmd.angular.z = 0.2
+    elif min(obstacles[150:200]) < 0.5:
+        cmd.linear.x = 0.1
+        cmd.angular.z = 0.2
+    elif min(obstacles[-250:-150]) < 0.5:
+        cmd.linear.x = 0.1
+        cmd.angular.z = -0.2
     else:
-        cmd.linear.x = 0.3
+        cmd.linear.x = 0.1
     commandPublisher.publish(cmd)
 
 # call the move_command at a regular frequency:
