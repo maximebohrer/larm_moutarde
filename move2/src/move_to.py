5e6dde6f8a06eb72f602b0e71c946aaae67b5b17
Nathanaël <toto@toto.fr>
2022-01-10 11:04:30 +0100

    ok

--- move2/src/move_to.py
+++ move2/src/move_to.py
@@ -1,2 +1,40 @@
 #!/usr/bin/python3
 
+import math, rospy
+from geometry_msgs.msg import Twist, PoseStamped
+
+# Initialize ROS::node
+rospy.init_node('move', anonymous=True)
+
+# commandPublisher = rospy.Publisher(
+#     '/cmd_vel_mux/input/navi',
+#     Twist, queue_size=10
+# )
+
+# call the move_command at a regular frequency:
+#"t = rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
+
+# Publish velocity commandes:
+# def move_command(data):
+#     # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
+#     cmd= Twist()
+#     cmd.linear.x= 0.1
+#     commandPublisher.publish(cmd)
+#     compteur += 1
+#     if compteur == 10:
+#         t.shutdown()
+        
+def callback(data):
+    print(str(data))
+    print("yolo")
+
+
+sus = rospy.Subscriber("/goal", PoseStamped, callback)
+
+
+
+
+
+# spin() enter the program in a infinite loop
+print("Start move.py")
+rospy.spin()
