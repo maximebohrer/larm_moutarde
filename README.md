e3489d07f6d28b264adb8f1d6d295da539cce81d
maximebohrer <maxime21.bohrer@gmail.com>
2022-01-16 20:03:05 +0100

    ok

--- README.md
+++ README.md
@@ -1,27 +1,78 @@
-# larm_moutarde
+LARM - Moutarde - Channenge 2
+=============================
 
-Branche pour le challenge 2 : The goal of the challenge is to demonstrate the capability the robot has to map an environment and to retrieve specific objects on it.
+The goal of the challenge is to demonstrate the capability the robot has to map an environment and to retrieve specific objects on it.
  
- /!\ Le projet nécessite les paquets de mb6-tbot
+/!\ The project depends on `mb6-tbot`
 
-Optionnal Features :
-    1 . Rviz
-    information is returned to rviz thanks to the challenge2.launch wich launch rviz with the configuration saved in the file config_ch2.rviz
-    
-    2 . shape of map 
-    The map is fully handled by the gmapping node
-    
-    3 . 2d-version-bottle
-    Our model has been specically trained to recognize black bottle of Nuka Cola®. We used a cascade classifier with Local Bynary Patterns (LBP). In order to improve the efficiency of the classifier, we trained one to recognize upright bottles and one for lying ones because of the signicative change of the shape of the object when the bottle is lying on the floor. We also generated our data base using the camera of the robot (intel realsense D435i) to obtain images as close as possible of the images that will be seen in the rosbag or by the robot when in action.
-    
-    4 . Position of bottle
-    We have used several technique in order to increase the precision of the bottle's position. First, we used the image_geometry library to translate the postion of the bottle in the image (in pixels) to the 3D plan of the camera. We also saved the time stamp of the image to give it back placing the marker. It permit the tf to do the transformation with the tf tree of the instant where the image was taken in order to compensate some latency between when the image is taken and when the marker is placed. The latency may appear with computing time.
-    
-    5 . No redundancy
-    It hasn't been dealt with yet.
-    
-    6 & 7 . All and only all bottle 
-    See the point 3
-    
-    8 . Service
-    It hasn't been dealt with yet. 
+
+Detection in the image
+----------------------
+
+To detect the bottles in an image from the camera, we trained a cascade classifier.
+
+Our model has been specically trained to recognize black bottles of Nuka Cola®. We used a cascade classifier with Local Bynary Patterns (LBP). We also generated our own large data base using the camera of the robot (intel realsense D435i) to obtain images as close as possible to the images that will be seen in the rosbag or by the robot.
+
+
+Processing the data from the camera
+-----------------------------------
+
+First, the distance from the camera to the bottle is calculated using the depth image. Then, the 3D coordinates of central point of the bottle in the camera frame is calculated using ROS' image_geometry library. Finally, the coordinates are converted in the map frame using the transformations published on tf.
+
+The first verification that we do to filter out false alarms is to check if the size of the bottle is coherent.
+
+This process is explained in more details in the python script `bottle_detector.py`
+
+
+Keeping track of the bottles
+----------------------------
+
+The `Bottle` class contains all the code used to keep track of the position of the bottles.
+
+For each frame, the main class passes the list of points it found to the Bottle class. For each point, if a bottle that is very close already exists, it is updated: its coordinates are adjusted. Otherwise, it is created. A newly created bottle has to be detected and updated enough times before beeing published. It is then marked as listed. Similarily, if a bottle that is not listed is not detected for too long, it is considered as a false alarm and is destroyed, without having been published.
+
+This process is explained in more details in the python script `bottle_detector.py`
+
+
+Optionnal Features
+------------------
+
+1. Rviz
+
+Information is returned to rviz thanks to the `challenge2.launch` which launches rviz with the configuration saved in the file `config_ch2.rviz`. The map, the laser, the bottle markers and an image containing the detected bottles are shown.
+
+2. Shape of map 
+
+The map is fully handled by the gmapping node.
+
+3. 2d-version-bottle
+
+We trained our model with black bottles, so it only detects the black bottles. See the "Detection in the image" paragraph.
+
+4. Position of bottle
+
+We have used several technique in order to increase the precision of the bottle's position. First, we used the image_geometry library to translate the postion of the bottle in the image (in pixels) to the 3D plan of the camera, rather than doing the trigonometry calculations by ourselves. We also saved the time stamp of the image to give it back when doing the transformations and placing the marker. This way, the transformations are calculated with the tf tree of the instant where the image was taken, and there is no ambiguity, even if there is some latency due to computiong time.
+
+5. The position of the bottle is streamed one and only one time
+
+See the "Keeping track of the bottles" paragraph. Each time a bottle is detected, its position is adjusted, by taking the average of the last few positions. By default, the marker is updated so that rviz can display it at the correct place: it is re-published with the SAME ID, so it is not new marker. If you want each marker to be published only one time, you can change the `publish_updates` variable to `False` in the `bottle_detector.py` script, line 108.
+
+6. All the bottles are detected
+
+Our model works pretty well on standing bottles. Unfortunately, the second model that we tried to train for lying bottles still has trouble seeing them, but we will work on that for the challenge 3.
+
+7. Only the bottle are detected
+
+Thanks to the various filters (see the "Processing the data from the camera" paragraph and the "Keeping track of the bottles" paragraph), we've had very few false alarms in the test rosbag.
+
+8. Service
+
+In a terminal, type:
+```bash
+rosservice call /print_bottles
+```
+You shold get an answer like
+```
+success: True
+message: "3 bottle(s) [2.001 -0.349 0.105] [-0.09 1.545 0.114] [-1.354 -0.849 0.128] "
+```
---
+++ grp-moutarde/src/scripts/bottle_detector.py
@@ -0,0 +1,208 @@
+#!/usr/bin/python3
+
+import rospy, rospkg, cv2, numpy as np, tf, image_geometry, message_filters
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import PointStamped
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge
+from kobuki_msgs.msg import Led
+from std_srvs.srv import Trigger
+from math import sqrt, inf
+
+
+class Node:
+    def __init__(self):
+        rospack = rospkg.RosPack()
+        self.pkg_path = rospack.get_path('grp-moutarde')
+        self.bridge = CvBridge()
+        self.classifier_debouts = cv2.CascadeClassifier(self.pkg_path + "/src/scripts/cascade1_deb.xml")
+        #self.classifier_couches = cv2.CascadeClassifier("/home/grp-moutarde/Bureau/train_cascade/cascade3_c27.xml")
+        self.cam_info = CameraInfo()
+        self.nb_detections = 0
+        self.working = False
+
+        # Publishers
+        self.publisher_led = rospy.Publisher('/mobile_base/commands/led1', Led, queue_size=10)
+        self.publisher_image = rospy.Publisher('/image_detection', Image, queue_size=10)
+
+        # Listeners
+        self.camera_listener = message_filters.TimeSynchronizer([
+            message_filters.Subscriber("/camera/color/image_raw", Image),
+            message_filters.Subscriber("/camera/aligned_depth_to_color/image_raw", Image),
+            message_filters.Subscriber("/camera/color/camera_info", CameraInfo)
+            ], 10)
+        self.camera_listener.registerCallback(self.callback)
+
+        # TF listener
+        self.tf_listener = tf.TransformListener()
+    
+    # This functions gets called each time data is received from the camera. The TimeSynchronizer takes care of using 3 messages which have the same time stamp.
+    def callback(self, color, depth, camera_info):
+        if not self.working:
+            self.working = True
+            color_img = self.bridge.imgmsg_to_cv2(color, desired_encoding='passthrough')
+            depth_img = self.bridge.imgmsg_to_cv2(depth, desired_encoding='passthrough')
+
+            # Detection in the frame and conversion to 3D coordinates
+            gray = cv2.cvtColor(color_img, cv2.COLOR_RGB2GRAY)
+            objets_debouts = self.classifier_debouts.detectMultiScale(gray, 1.1, minNeighbors=3)
+            points = []
+            for (x, y, w, h) in objets_debouts:
+                dist = self.find_distance(depth_img, x, y, w, h)
+                h_times_dist = h * dist # The appearant height is inversely proportional to the distance, so h*dist is a constant. Comparing it to its expected value, which turns out to be between 140 and 180, gives us a way to filter out false alarms.
+                if h_times_dist >= 140 and h_times_dist <= 180:
+                    cv2.rectangle(color_img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                    point = self.find_center_point(camera_info, x, y, w, h, dist)
+                    points.append(self.create_point_in_map_frame(point, color.header.stamp))
+                    print(h_times_dist)
+            self.update_led(len(points))
+
+            # Create and update the position of the bottles
+            Bottle.update(points, color.header.stamp)
+        
+            image_detection = self.bridge.cv2_to_imgmsg(color_img, encoding="rgb8")
+            self.publisher_image.publish(image_detection)
+            self.working = False
+
+    # Calculate the distance between the camera and the rectangle using the depth image
+    def find_distance(self, depth_img, x, y, w, h):
+        center_depth = depth_img[y+h//3:y+2*h//3, x+w//3:x+2*w//3]      # get a small rectangle in the middle of the detected bottle
+        dist = np.median(center_depth)                                  # take the median of this small rectangle to avoid beeing affected by extreme values like the background or parasites
+        return dist / 1000                                              # convert millimeters to meters
+
+    # Calculates the 3D coordinates of the center of the rectangle in the camera frame
+    def find_center_point(self, camera_info, x, y, w, h, dist):
+        cam_model = image_geometry.PinholeCameraModel()
+        cam_model.fromCameraInfo(camera_info)
+        ray = np.array(cam_model.projectPixelTo3dRay((x+w//2, y+h//2))) # convert the pixel coordinates of the center point to 3D coordinates in the camera frame (at a distance of 1)
+        point = ray * (dist + 0.022)                                    # we then multiply by the distance that we calculated to which we add about half of the bottle's diameter.
+        return point
+    
+    # Convert tuple in camera frame to PointStamped in map frame
+    def create_point_in_map_frame(self, point_in_camera_frame, stamp):
+        msg = PointStamped()
+        msg.header.stamp = stamp
+        msg.header.frame_id = "camera_color_optical_frame"
+        msg.point.x, msg.point.y, msg.point.z = point_in_camera_frame
+        return self.tf_listener.transformPoint("map", msg)
+
+    # Light up the robot LED if bottles are detected
+    def update_led(self, nb):
+        if self.nb_detections != nb:
+            self.nb_detections = nb
+            msg = Led()
+            msg.value = self.nb_detections
+            if msg.value > 3: msg.value = 3
+            self.publisher_led.publish(msg)
+    
+
+class Bottle:
+    # Static variables
+    bottles = []
+    id_counter = 0
+
+    # Parameters
+    required_detections = 4             # Number of detections required for a bottle to be listed
+    max_alive_time = 4                  # Max number of frames the bottle is kept alive for without being detected before it is listed
+    detection_distance = 0.3            # distance under which the bottles are considered the same
+    publish_updates = True              # Each time a bottle is detected, its position is adjusted. This variable determines if those adjustments should be published. If false, the marker will only be sent once. If true, the marker will be updated (re-published withe the SAME ID) each time the bottle is detected.
+
+    # Bottle publisher
+    publisher_bottle = rospy.Publisher('/bottle', Marker, queue_size=10)
+
+    # Creation of a new bottle
+    def __init__(self, point):
+        Bottle.id_counter += 1
+        self.id = Bottle.id_counter
+        self.point = point              # PointStamped in map frame
+        self.detection_counter = 0      # Number of times the bottle has been detected
+        self.kill_countdown = 0         # Number of frames the bottle has been alive for
+        self.listed = False             # Stays False until the bottle has been detected enough times. Before that the bottle will not be published
+    
+    # Modification of an existing bottle
+    def modify(self, point, stamp):
+        x = (4 * self.point.point.x + point.point.x) / 5    # To smooth out the position of the bottle, we take the average
+        y = (4 * self.point.point.y + point.point.y) / 5    # of the five last values.
+        z = (4 * self.point.point.z + point.point.z) / 5
+        self.point = point
+        self.point.point.x, self.point.point.y, self.point.point.z = x, y, z
+        if not self.listed:
+            self.detection_counter += 1
+            self.kill_countdown = 0
+            if self.detection_counter >= Bottle.required_detections:
+                self.listed = True
+                self.publish(stamp)
+        elif Bottle.publish_updates:
+            self.publish(stamp)
+
+    # Static function which runs each frame. The points variable contains all the PointStamped detected in the current frame.
+    def update(points, stamp):
+        for b in Bottle.bottles:
+            nearest_point = None
+            lowest_dist = inf
+            for p in points:
+                dist = b.distance_to(p)
+                if dist < lowest_dist:
+                    lowest_dist = dist
+                    nearest_point = p
+            if lowest_dist < Bottle.detection_distance:
+                b.modify(nearest_point, stamp)          # This point is considered to correspond to an existing bottle. The bottle is adjusted with the coordinates of the new point.
+                points.remove(nearest_point)            # Do not compare this point again to the next bottles. That way if multiple points are detected in one frame, they will all necessarily be treated as different bottles, even if they are close.
+
+        for p in points:
+            new_bottle = Bottle(p)
+            Bottle.bottles.append(new_bottle)           # The remaining points, which do not correspond to any existing bottle, are added as new bottles.
+        
+        # Kill all the bottles that have not been listed and not detected for too long. This filters out false alarms
+        for b in Bottle.bottles:
+            if not b.listed:
+                b.kill_countdown += 1
+                if b.kill_countdown >= Bottle.max_alive_time:
+                    Bottle.bottles.remove(b)
+
+    # Calculate the distance between the bottle and a PointStamped
+    def distance_to(self, point):
+        return sqrt((self.point.point.x - point.point.x)**2 + (self.point.point.y - point.point.y)**2 + (self.point.point.z - point.point.z)**2)
+
+    # Publish the bottle as a marker
+    def publish(self, stamp):
+        msg = Marker()
+        msg.header.frame_id = "map"
+        msg.header.stamp = stamp
+        msg.ns = "Bouteille"
+        msg.id = self.id
+        msg.type = 1
+        msg.action = 0
+        msg.pose.position = self.point.point
+        msg.pose.orientation.w = 1
+        msg.scale.x, msg.scale.y, msg.scale.z = [0.1, 0.1, 0.1]
+        msg.color.r, msg.color.g, msg.color.b, msg.color.a = [0, 255, 0, 255]
+        #msg.lifetime.secs, msg.lifetime.nsecs = [2, 0]
+        Bottle.publisher_bottle.publish(msg)
+    
+    # Static function that returns the list of PointStamped correponding to all the listed bottles
+    def get_bottles():
+        bottles = []
+        for b in Bottle.bottles:
+            if b.listed:
+                bottles.append(b.point)
+        return bottles
+
+
+class Service:
+    def __init__(self):
+        rospy.Service("print_bottles", Trigger, self.bottle_srv)
+    
+    def bottle_srv(self, req):
+        bottles = Bottle.get_bottles()
+        answer = str(len(bottles)) + " bottle(s) "
+        for b in bottles:
+            answer += "[" + str(round(b.point.x, 3)) + " " + str(round(b.point.y, 3)) + " " + str(round(b.point.z, 3)) + "] "
+        return (True, answer)
+
+
+rospy.init_node('bottle_detector', anonymous=True)
+node = Node()
+service = Service()
+print("Start bottle_detector.py")
+rospy.spin()
